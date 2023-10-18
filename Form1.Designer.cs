2f2b96afe7e6c5a82512c58d61d0c5c74739bb81
Tien-Thanh-MGH <nguyentienthanh1136@gmail.com>
2023-10-18 13:24:58 +0700

    uopppppppppp

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(203, 362);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(150, 29);
+            this.checkBox1.Size = new System.Drawing.Size(80, 29);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "nan";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
